7636831664c78f071d16a8b7eb452dfcb1d3cba4
dvazquez <dvazquez@madpor1041.exis.local>
2020-05-29 08:40:17 +0200

    commit inicial creación proyecto

---
+++ calculadoraRest/src/main/java/com/sanitas/calculadora/CalculadoraRestApplication.java
@@ -0,0 +1,13 @@
+package com.sanitas.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRestApplication.class, args);
+	}
+
+}
